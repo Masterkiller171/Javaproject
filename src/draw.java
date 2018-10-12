5c3c75540664b8905cd04abb95e1c871b13e511d
Youri Bontekoe <youribontekoe@gmail.com>
2018-10-12 15:46:47 +0200

    6

--- src/draw.java
+++ src/draw.java
@@ -1,78 +1,93 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class draw {
     private long value = 0; //Is the amount of clicks
-    private Label lb; //Label showing the amount of clicks
-    private Button but; //Button to click to increase the value of clicks
-    private Button clckmulti; //Button for the multiplier click
-    private Button autoclck; //Button for the auto click function
-    private Frame f = new Frame(); //Creating a frame for the grid
+    private long totalValue = 0;
+    private JButton doubler;
+    private JPanel Panel;
+    private JLabel lb; //Label showing the amount of clicks
+    private JLabel stats;
+    private JButton but; //Button to click to increase the value of clicks
+    private JButton clckmulti; //Button for the multiplier click
+    private JButton gamble; //Button for the auto click function
+    private JFrame p = new JFrame(); //Creating a frame for the grid
     private int muliplierPrice = 10; //Is the default price to buy a multiplier
     private int multiplier = 1; //Is the default multiplier for the clicks
-    private boolean auto = false; //Boolean for wheter the auto click upgrade has been bought
+    private int doubles = 1;
+    private int doublePrice = 50;
 
 //Constructs all classes to main
     public void controlGUI(){
         buildGUI();
         buildClickGUI();
-        autoclk();
     }
 //Building the grid of the GUI
     private void buildGUI(){
+        p = new JFrame("clicker");
+        p.setSize(1000,1000);
+        p.setLayout(new GridLayout(0, 2));
+        p.setVisible(true);
 
-        f.setSize(1000,1000);
-        f.setLayout(new GridLayout(10, 1));
-        f.setVisible(true);
+        Panel = new JPanel();
+        Panel.setLayout(new FlowLayout());
+
     }
 
 //Building the GUI it self
     private void buildClickGUI(){
 
-        lb = new Label("click");
-        but = new Button("click dis");
-        but.setBackground(Color.red);
-        lb.setAlignment(1);
-        f.add(lb);
-        f.add(but);
 
-        clckmulti = new Button("Buy Click multiplier, cost: " + this.muliplierPrice + " Multiplier: " + this.multiplier);
-        f.add(clckmulti);
+        lb = new JLabel("click");
+        but = new JButton("click dis");
+        stats = new JLabel("<html> Total clicks: " + this.totalValue + "<br> multiplier level: " + this.multiplier + " </html>");
+        p.add(lb);
+        p.add(stats);
+        p.add(but);
 
-        autoclck = new Button("Auto clicker");
-        f.add(autoclck);
+        clckmulti = new JButton("Buy Click multiplier, cost: " + this.muliplierPrice + " Multiplier: " + this.multiplier);
+        p.add(clckmulti);
+
+        doubler = new JButton("Doubles the current multiplier, cost: " + this.doublePrice + " doubler level: " + this.doubles);
+        p.add(doubler);
+
+        gamble = new JButton("Gamble");
+        p.add(gamble);
 
         but.addActionListener(this::actionPerformed);
         clckmulti.addActionListener(this::actionPerformed);
-        autoclck.addActionListener(this::actionPerformed);
+        doubler.addActionListener(this::actionPerformed);
     }
 
     //Actions performed when a button is pressed
     private void actionPerformed(ActionEvent e){
         if(e.getSource() == but){
             lb.setText("clicks: " + this.value);
-            value += this.multiplier;
+
+            value += this.multiplier * this.doubles;
+            totalValue += multiplier * doubles;
+            stats.setText("<html> Total clicks: " + this.totalValue + "<br> multiplier level: " + this.multiplier + " </html>");
+
         }else if(e.getSource() == clckmulti){
             if(this.value > this.muliplierPrice) {
+
                 value -= this.muliplierPrice;
                 muliplierPrice *= 2;
                 this.multiplier++;
+
                 lb.setText("clicks: " + this.value);
-                clckmulti.setLabel("Buy Click multiplier, cost: " + this.muliplierPrice + " Multiplier: " + this.multiplier);
-            }else if(e.getSource() == autoclck){
-                auto = true;
+                clckmulti.setText("Buy Click multiplier, cost: " + this.muliplierPrice + " Multiplier: " + this.multiplier);
+
+            }else if(e.getSource() == doubler){
+                if(this.value > this.doublePrice) {
+                    this.doubles++;
+                    this.value -= this.doublePrice;
+                    doublePrice *= 3;
+                    doubler.setText("Doubles the current multiplier, cost: " + doublePrice + " doubler level: " + this.doubles);
+                }
             }
         }
 }
 
-//Clicks automaticly when you buy the upgrade
-    private void autoclk(){
-        if(auto){
-            long oldTime = System.currentTimeMillis();
-            while(System.currentTimeMillis() - oldTime < 1000){
-                autoclck.setLabel(value++ + "test");
-                oldTime = System.currentTimeMillis();
-            }
-        }
-    }
 }
